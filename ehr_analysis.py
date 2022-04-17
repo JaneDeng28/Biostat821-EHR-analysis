dd254e4b6c8e0c913d746a4434869208f6b939cc
JaneDeng28 <91543834+JaneDeng28@users.noreply.github.com>
2022-04-17 13:58:46 -0500

    Update ehr_analysis.py

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -1,77 +1,54 @@
-from datetime import date, timedelta, datetime
+from datetime import datetime
 from typing import Union
+from typing import List
 
 """Data Parsing"""
 
+def parse_patient(patient: str) -> List[List[str]]: 
+    with open(patient, "r") as file: 
+        rows = file.readlines() 
+        lists = [] 
+    for row in rows: 
+        row = row.strip() 
+        row = row.split("\t") 
+        lists.append(row)
+    return lists 
 
-def parse_data(filename: str) -> list[str]:
-    result = []
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        headers = lines[0].encode("utf-8").decode("utf-8-sig").strip().split()  # O(1)
-        for line in lines[1:]:  # N - 1, start from 2 nd line
-            d = dict()  # O(1)
-            data = line.strip().split("\t")  # O(1)
-            for i in range(len(data)):  # M times -> constant : 7
-                key = headers[i]  # O(1)
-                d[key] = data[i]  # O(1)
-            result.append(d)  # O(1)
-    return result
-    # O(1) + O(1) + O(1) + O(1) + (N - 1) * O(1) + (N - 1) * O(1) + (N - 1) * (M * O(1) + M * O(1)) + (N - 1) * O(1)
-    # (N - 1) * (3 * O(1) + (M * O(1) + M * O(1))
-    # -> (N - 1) * 2M * O(1) - > (N - 1) * 2M -> N * M -> O(N * M)
-    # -> O(N)
+def parse_lab(lab: str) -> List[List[str]]: 
+    with open(lab, "r") as file: 
+        rows = file.readlines() 
+        lists = [] 
+    for row in rows: 
+        row = row.strip() 
+        row = row.split("\t")
+        lists.append(row)
+    return lists 
 
-
-def num_older_than(age_old: Union[int, float], dataname: str) -> int:
-    num = 0
-    for record in dataname:  # N
-        birth = record["PatientDateOfBirth"]  # 0(1)
-        b_str = datetime.strptime(birth, "%Y-%m-%d %H:%M:%S.%f")  # 0(1)
-        age_new = (datetime.today() - b_str) / timedelta(days=365.2425)  # 0(1)
-        if age_new > age_old:  # 0(1)
-            num += 1  # 0(1)
-    return num
-
-
-# 5 * N -> 0(N)
+def num_older_than(age: float, patient: List[List[str]]) -> int:
+    now = datetime.today() 
+    num = 0 
+    for i in range(1, len(patient) - 1): 
+        born = datetime.strptime(patient[i][2], "%Y-%m-%d %H:%M:%S.%f") 
+        if (now.year - born.year - ((now.month, now.day) < (born.month, born.day))) > age: 
+            num += 1 
+    return num  
 
 
-def sick_patients(lab:str, gt_lt:str, value:float, lablist:list[str]) -> list[str]:
-    lab_name = 0
-    lab_value = 0
+def sick_patients(lab: str, gt_lt: str, value: float, lablist: List[List[str]] ) -> List[str]:
+    patid = set()
+    for i in range(1, len(lablist)-1):
+        if lablist[i][2] == lab:
+            if gt_lt == ">":
+                if float(lablist[i][3]) > value:
+                    patid.add(lablist[i][0])
+            elif gt_lt == "<":
+                if float(lablist[i][3]) < value:
+                    patid.add(lablist[i][0])
+            else: 
+                raise ValueError("Please input vaild '<' or '>'") 
+    return list(patid)
 
-    for i in range(len(lablist[0])):  # 6 times
-        if lablist[0][i] == "LabName":  # O(1)
-            lab_name = i
-        if lablist[0][i] == "LabValue":  # O(1)
-            lab_value = i
-
-    Larger = []
-    Smaller = []
-
-    for i in range(1, len(lablist)):  # N times
-        if lablist[i][lab_name] == lab:  # O(1)
-            if gt_lt == ">" and float(lablist[i][lab_value]) > value:
-                Larger.append(lablist[i][0])
-            elif gt_lt == "<" and float(lablist[i][lab_value]) < value:
-                Smaller.append(lablist[i][0])
-
-    idlist = []
-    newlist = []
-
-    if Larger != [] and gt_lt == ">":
-        idlist = Larger
-    if Smaller != [] and gt_lt == "<":
-        idlist = Smaller
-    for i in idlist:
-        if i not in newlist:
-            newlist.append(i)
-
-    return newlist
-
-
-def patient_age(patients:list[str], patient_id:list[str]) -> int:
+def patient_age(patients:List[str], patient_id:List[str]) -> int:
     """
     A function that computes the age at first admission of any given patient
     @param patients: input data
@@ -84,14 +61,10 @@ def patient_age(patients:list[str], patient_id:list[str]) -> int:
         if patient[0] == patient_id:
             return round((date0 - birth).days / 365, 1)
 
-
+"""
 if __name__ == "__main__ ":
-    patient_data = parse_data(
-        "/Users/jane/2022/SP2022/BIOSTAT821/PatientCorePopulatedTable.txt"
-    )
-    print(num_older_than(51.2, patient_data))
-
-    lab_data = parse_data(
-        "/Users/jane/2022/SP2022/BIOSTAT821/LabsCorePopulatedTable.txt"
-    )
-    print(sick_patients("METABOLIC: ALBUMIN", ">", 4.0, lab_data))
+"""
+patient_data = parse_patient("/Users/jane/2022/SP2022/BIOSTAT821/PatientCorePopulatedTable.txt")    
+print(num_older_than(51.2, patient_data))
+lab_data = parse_lab("/Users/jane/2022/SP2022/BIOSTAT821/LabsCorePopulatedTable.txt")    
+print(sick_patients("METABOLIC: ALBUMIN", ">", 4.0, lab_data))
