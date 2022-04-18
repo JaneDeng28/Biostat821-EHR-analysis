98747453c8c513b820bd31679f50a6af73e4019b
JaneDeng28 <91543834+JaneDeng28@users.noreply.github.com>
2022-04-17 19:49:33 -0500

    Update test_ehr_analysis.py

--- test_ehr_analysis.py
+++ test_ehr_analysis.py
@@ -1,20 +1,27 @@
 import ehr_analysis
-import pytest
+
+patient_data = ehr_analysis.parse_patient(
+    "https://github.com/JaneDeng28/Biostat821-EHR-analysis/blob/077fbfefd6f2ad1c982fa3de946585e4b9c7040a/test_patient.txt"
+)
+lab_data = ehr_analysis.parse_lab(
+    "https://github.com/JaneDeng28/Biostat821-EHR-analysis/blob/077fbfefd6f2ad1c982fa3de946585e4b9c7040a/test_lab.txt"
+)
+
 
 def test_parse_patient():
     """
     A test function that checks whether the parse_data function returns the correct list for patient data
     """
     assert [
-        "64182B95-EB72-4E2B-BE77-8050B71498CE",
+        "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
         "Male",
-        "1952-01-18 19:51:12.917",
-        "African American",
-        "Separated",
-        "English",
-        "13.03",
-    ] in ehr_analysis.patient_data
-    assert len(ehr_analysis.patient_data) == 101
+        "1947-12-28 02:45:40.547",
+        "Unknown",
+        "Married",
+        "Icelandic",
+        "18.08",
+    ] in patient_data
+    assert len(patient_data) == 1
 
 
 def test_parse_lab():
@@ -22,33 +29,28 @@ def test_parse_lab():
     A test function that checks whether the parse_data function returns the correct list for lab data
     """
     assert [
-        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+        "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
         "1",
-        "CBC: MCH",
-        "35.8",
-        "pg",
-        "1992-06-30 03:50:11.777",
-    ] in ehr_analysis.lab_data
-    assert len(ehr_analysis.lab_data) == 111484
+        "URINALYSIS: RED BLOOD CELLS",
+        "1.8",
+        "rbc/hpf",
+        "1992-07-01 01:36:17.910",
+    ] in lab_data
+    assert len(lab_data) == 2
 
 
 def test_num_older_than():
     """
     A test function that checks whether the num_older_than function returns the correct value
     """
-    assert ehr_analysis.num_older_than(50, ehr_analysis.patient_data) == 78
+    assert ehr_analysis.num_older_than(50, patient_data) == 78
 
 
 def test_sick_patients():
     """
     A test function that checks whether the sic_patients function returns the correct value
     """
-    assert (
-        ehr_analysis.sick_patients(
-            ehr_analysis.lab_data, "METABOLIC: ALBUMIN", ">", 5.9
-        )
-        == 42
-    )
+    assert ehr_analysis.sick_patients(lab_data, "METABOLIC: ALBUMIN", ">", 5.9) == 42
 
 
 def test_patient_age():
@@ -56,8 +58,6 @@ def test_patient_age():
     A test function that checks whether the patient_age function returns the correct age at first admission
     """
     assert (
-        ehr_analysis.patient_age(
-            ehr_analysis.patient_data, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F"
-        )
+        ehr_analysis.patient_age(patient_data, "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F")
         == 74
     )
