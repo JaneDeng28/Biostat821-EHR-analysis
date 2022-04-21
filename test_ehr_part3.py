3495aa3341eb84d76cc034ec40c4afdca222a8a9
JaneDeng28 <91543834+JaneDeng28@users.noreply.github.com>
2022-04-21 13:12:12 -0500

    Update test_ehr_part3.py

--- test_ehr_part3.py
+++ test_ehr_part3.py
@@ -1,34 +1,45 @@
 """Test class and object-oriented functions."""
 import ehr_analysis
 import ehr_part3
-from datetime import datetime
 
 """
-patient_data = ehr_analysis.parse_patient("test_patient.txt")
-lab_data = ehr_analysis.parse_lab("test_lab.txt")
-"""
-
 patient_a = ehr_part3.Patient(
     PatientID="FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
     Gender="Male",
     DOB="1947-12-28 02:45:40.547",
     Race="Unknown",
-)
+)"""
 
+patient_a = ehr_part3.Patient(
+    PatientID="FB2ABB23-C9D0-4D09-8464-49BF0B982F0F", 
+    patfilename="test_patient.txt"
+)
+"""
 patient_b = ehr_part3.Patient(
     PatientID="7FD13988-E58A-4A5C-8680-89AC200950FA",
     Gender="Male",
     DOB="1965-07-12 15:41:20.523",
     Race="White",
 )
-
+"""
+patient_b = ehr_part3.Patient(
+    PatientID="7FD13988-E58A-4A5C-8680-89AC200950FA",
+    patfilename="test_patient.txt"
+)
+"""
 patient_c = ehr_part3.Patient(
     PatientID="B39DC5AC-E003-4E6A-91B6-FC07625A1285",
     Gender="Female",
     DOB="1935-11-03 21:07:09.040",
     Race="White",
 )
+"""
+patient_c = ehr_part3.Patient(
+    PatientID="B39DC5AC-E003-4E6A-91B6-FC07625A1285",
+    patfilename="test_patient.txt"
+)
 
+"""
 lab_a1 = ehr_part3.Lab(
     PatientID="1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
     LabName="CBC: ABSOLUTE LYMPHOCYTES",
@@ -36,7 +47,14 @@ lab_a1 = ehr_part3.Lab(
     LabUnits="%",
     LabDateTime="1992-06-30 09:39:02.830",
 )
-
+"""
+lab_a1 = ehr_part3.Lab(
+    PatientID="1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+    LabName="CBC: ABSOLUTE LYMPHOCYTES",
+    LabDateTime="1992-06-30 09:39:02.830",
+    labfilename="test_lab.txt"
+)
+"""
 lab_a2 = ehr_part3.Lab(
     PatientID="81C5B13B-F6B2-4E57-9593-6E7E4C13B2CE",
     LabName="CBC: ABSOLUTE LYMPHOCYTES",
@@ -44,7 +62,14 @@ lab_a2 = ehr_part3.Lab(
     LabUnits="%",
     LabDateTime="2005-07-27 12:03:25.640",
 )
-
+"""
+lab_a2 = ehr_part3.Lab(
+    PatientID="81C5B13B-F6B2-4E57-9593-6E7E4C13B2CE",
+    LabName="CBC: ABSOLUTE LYMPHOCYTES",
+    LabDateTime="2005-07-27 12:03:25.640",
+    labfilename="test_lab.txt"
+)
+"""
 lab_a3 = ehr_part3.Lab(
     PatientID="220C8D43-1322-4A9D-B890-D426942A3649",
     LabName="CBC: ABSOLUTE LYMPHOCYTES",
@@ -60,7 +85,15 @@ lab_b = ehr_part3.Lab(
     LabUnits="rbc/hpf",
     LabDateTime="2011-02-01 21:46:21.930",
 )
+"""
 
+lab_b = ehr_part3.Lab(
+    PatientID="220C8D43-1322-4A9D-B890-D426942A3649",
+    LabName="URINALYSIS: RED BLOOD CELLS",
+    LabDateTime="2011-02-01 21:46:21.930",
+    labfilename="test_lab.txt"
+)
+"""
 lab_c = ehr_part3.Lab(
     PatientID="C242E3A4-E785-4DF1-A0E4-3B568DC88F2E",
     LabName="URINALYSIS: PH",
@@ -68,7 +101,13 @@ lab_c = ehr_part3.Lab(
     LabUnits="no unit",
     LabDateTime="1987-10-10 12:40:17.297",
 )
-
+"""
+lab_c = ehr_part3.Lab(
+    PatientID="C242E3A4-E785-4DF1-A0E4-3B568DC88F2E",
+    LabName="URINALYSIS: PH",
+    LabDateTime="1987-10-10 12:40:17.297",
+    labfilename="test_lab.txt"
+)
 
 def test_num_older_than():
     """Test function num_older_than."""
@@ -76,7 +115,6 @@ def test_num_older_than():
         ehr_analysis.num_older_than(age=100, patients=[patient_a, patient_b, patient_c])
         == 0
     )
-
 
 def test_sick_patients():
     """Test function sick_patients."""
@@ -91,7 +129,6 @@ def test_sick_patients():
         )
         == 0
     )
-
 
 def test_patient_age():
     """Test function patient_age."""
